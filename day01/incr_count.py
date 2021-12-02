8b3333d5c38ea8d2a72f00adc975ca3f2f949cb9
Quentin Bitram <Qu-Bit@users.noreply.github.com>
2021-12-02 09:39:12 +0100

    day01-Q2 solved

--- day01/incr_count.py
+++ day01/incr_count.py
@@ -1,8 +1,27 @@
-"""AoC 2021: day01/puzzle1
+"""AoC 2021: day01/
+    + puzzle1
+    + puzzle2
 """
+
+import numpy as np
+
+# %% input data
 depth = np.genfromtxt('input.txt')
 len(depth)
 
+# %% puzzle 1
+print('---\npuzzle 1:')
 ddepth = np.diff(depth)
-print('number of successive measurements showing increased depth:', sum(ddepth > 0))
+print('*A:* number of successive measurements\n\t'
+      'showing increased depth:', sum(ddepth > 0))
 
+
+# %% puzzle 2
+print('---\npuzzle 2:')
+sumWin = 3
+cs3_dep = np.convolve(depth, [1.,]*sumWin, mode='valid')
+assert len(cs3_dep) == (len(depth) - sumWin + 1)
+
+ddepth = np.diff(cs3_dep)
+print('*A:* number of successive [%d-]sum-windows\n\t'
+      'showing increased depth:' % sumWin, sum(ddepth > 0))
